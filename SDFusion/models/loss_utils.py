a2d5090cac13cad60bfb16daa116588a03239d5c
root <root@109f1e038785451f90b0ac1632a0c0b000005G.devoc1zsu3le1g4iqhp2m1nnqf.jx.internal.cloudapp.net>
2024-05-09 13:12:21 +0000

    udpate

--- SDFusion/models/loss_utils.py
+++ SDFusion/models/loss_utils.py
@@ -49,10 +49,10 @@ def get_physical_loss(sdf, ply, ply_translation, ply_rotation,
                        move_type=None, move_samples=32, 
                        res=64,
                        scale_mode='max_extent',
-                       margin=1/256,
+                       margin=1/128,
                        use_bbox=False, linspace=False,
                        loss_collision_weight=1.0,
-                       loss_contact_weight=10000.0):
+                       loss_contact_weight=1.0):
     '''
     sdf: sdf values, (B, 1, res, res, res), multiple generated sdf with the same point cloud condition
     ply: point cloud, (1, 3, N)
--- SDFusion/scripts/test/plybbox2shape/test.sh
+++ SDFusion/scripts/test/plybbox2shape/test.sh
@@ -37,7 +37,8 @@ model_id='48876_1'
 cat="hinge_door"
 rotate_angle=$4
 
-# --loss_margin 0.00390625 1/256
+# 0.00390625 1/256
+# 0.0078125 1/128
 
 args="--name ${name} --logs_dir ${logs_dir} --gpu_ids ${gpu_ids} \
             --batch_size ${batch_size} --max_dataset_size ${max_dataset_size} \
@@ -47,11 +48,11 @@ args="--name ${name} --logs_dir ${logs_dir} --gpu_ids ${gpu_ids} \
             --total_iters ${total_iters} --dataroot ${dataroot} \
             --ply_rotate \
             --rotate_angle ${rotate_angle} \
-            --scale_mode volume \
+            --scale_mode max_extent \
             --ply_bbox_cond --cond_ckpt ${cond_ckpt} --load_iter ${load_iter} \
             --ddim_steps 50 --uc_ply_scale 2 --uc_bbox_scale 2 \
-            --loss_margin 0.00390625 \
-            --test_description margin256-debug \
+            --loss_margin 0.0078125 \
+            --test_description margin128 \
             --use_mobility_constraint "
             # --model_id ${model_id}"
 
