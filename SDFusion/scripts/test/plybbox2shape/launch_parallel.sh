835e8cfa3467e6ac5f6ad4e5c5befebae8003b7a
root <root@109f1e038785451f90b0ac1632a0c0b000005E.devoc1zsu3le1g4iqhp2m1nnqf.jx.internal.cloudapp.net>
2024-05-11 02:41:28 +0000

    update

--- SDFusion/scripts/test/plybbox2shape/launch_parallel.sh
+++ SDFusion/scripts/test/plybbox2shape/launch_parallel.sh
@@ -7,7 +7,7 @@ do
     start_idx=$((($i)*5))
     end_idx=$((($i+1)*5))
     bash scripts/test/plybbox2shape/test_guided_haoran_parallel.sh "slider_drawer-plybbox2shape-plyrot-lr0.00001" "${i}" "150000" "0" "${ply_scale}" "${bbox_scale}" "${start_idx}" "${end_idx}"
-    sleep 1800
+    sleep 180
 done
 done
 done
